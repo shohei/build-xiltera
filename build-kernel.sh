fa0789ff96f4f3d8f5997d72c1a94b4e4f3d3fc7
Hidemi Ishihara <hidemi@sweetcafe.jp>
2017-01-21 13:55:48 +0900

    Modify Enviroment for SDK
    Signed-off-by: Hidemi Ishihara <hidemi@sweetcafe.jp>

--- build-kernel.sh
+++ build-kernel.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build
 
-source /opt/poky/2.1.1/environment-setup-cortexa9hf-neon-poky-linux-gnueabi
+source /opt/poky/2.2.1/environment-setup-cortexa9hf-neon-poky-linux-gnueabi
 
 # Kernel
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
